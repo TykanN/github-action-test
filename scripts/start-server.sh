f09f9e999fe894dc280baeb0159471d0362bb20c
Theo / Taeyoon Kang <kang3603a@gmail.com>
2024-07-21 19:39:15 +0900

    feature: 코드디플로이 적용

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/spring_demo
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+# cd /home/ubuntu/spring_demo
+# sudo fuser -k -n tcp 8080 || true
+# nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop instagram-server || true
+docker rm instagram-server || true
+docker pull 471112870624.dkr.ecr.us-east-1.amazonaws.com/instagram-server:latest
+docker run -d --name instagram-server -p 8080:8080 471112870624.dkr.ecr.us-east-1.amazonaws.com/instagram-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
+
+
--- spring_demo/src/main/kotlin/com/example/demo/AppController.kt
+++ spring_demo/src/main/kotlin/com/example/demo/AppController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class AppController {
 	@GetMapping()
 	fun home(): String {
-		return "Hello, Tykan Docker World!!"
+		return "Hello, Tykan Docker + CodeDeploy World!!"
 	}
 }
